d68603b76f158dfb9dc50d8543a016156e0f3e87
kuldeepH17 <humbalkuldeep54@gamil.com>
2024-07-10 01:57:21 +0530

    chore: Remove unused contact form files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,17 @@
-/**
-* Template Name: iPortfolio
-* Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
-* Updated: Jun 29 2024 with Bootstrap v5.3.3
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
+//for the tab links//
+var tablinks =document.getElementsByClassName("tab-links");
+var tabcontents =document.getElementsByClassName("tab-contents");
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab"); 
+}
 
 (function() {
   "use strict";
